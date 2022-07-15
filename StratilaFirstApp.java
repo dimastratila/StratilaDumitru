5408d6a2ca24f87ea31a7b9b0c330bb1a691306e
Stratila Dumitru <stratila.mail@gmail.com>
2022-07-15 18:08:31 +0300

    Test gitignore v.2

--- StratilaFirstApp.java
+++ StratilaFirstApp.java
@@ -2,6 +2,7 @@ public class StratilaFirstApp{
 	public static void main(String[] args){
 		System.out.println("Henlo hooman");
 		System.out.println("Print another text");
+		System.out.println("testing something");
 		
 	}
 	
