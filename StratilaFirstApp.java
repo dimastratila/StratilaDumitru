e8a90d2080825788045151f46a60cab3de523ca4
Stratila Dumitru <stratila.mail@gmail.com>
2022-07-15 18:21:00 +0300

    test

--- StratilaFirstApp.java
+++
@@ -1,9 +0,0 @@
-public class StratilaFirstApp{	
-	public static void main(String[] args){
-		System.out.println("Henlo hooman");
-		System.out.println("Print another text");
-		System.out.println("testing something v.2");
-		
-	}
-	
-}
