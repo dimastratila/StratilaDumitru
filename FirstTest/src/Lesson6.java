d3cc03bffc790bcd7e25beeafd65f7183cec93f1
Stratila Dumitru <stratila.mail@gmail.com>
2022-07-22 18:17:12 +0300

    upload

---
+++ FirstTest/src/Lesson6.java
@@ -0,0 +1,16 @@
+public class Lesson6 {
+    public static void main(String[] args){
+        int a = 13;
+
+        if ((a%2==0) && (a>10)){
+            System.out.println("Numarul este par");
+        }
+
+        else if ((a%2!=0)&&(a<15)){
+            System.out.println("Numarul este impar");
+        }
+
+    }
+
+
+}
